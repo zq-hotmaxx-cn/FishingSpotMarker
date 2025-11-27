45b288518918d2bc73652041270c79afd7f8911b
HOTMAXX\SPzengquan <zengquan@hotmaxx.cn>
2025-11-27 14:56:56 +0800

    修改用户表字段属性

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -23,24 +23,24 @@ type User struct {
 	PasswordHash string `gorm:"type:varchar(256)" validate:"required, max=256"`
 
 	// 微信开放平台唯一标识（跨应用唯一）
-	UnionID string `gorm:"type:varchar(64); uniqueIndex; not null" validate:"required, len=64"`
+	UnionID string `gorm:"type:varchar(64); uniqueIndex" validate:"required, len=64"`
 	// 小程序唯一标识
-	OpenID string `gorm:"type:varchar(64); uniqueIndex; not null" validate:"required, len=64"`
+	OpenID string `gorm:"type:varchar(64); uniqueIndex" validate:"required, len=64"`
 
 	// 昵称
 	NickName string `gorm:"type:varchar(128); not null" validate:"required, min=1, max=128"`
 	// 头像
-	AvatarURL string `gorm:"type:varchar(512); not null" validate:"min=1, max=256"`
+	AvatarURL string `gorm:"type:varchar(512)" validate:"min=1, max=256"`
 	// 性别
 	Gender Gender `gorm:"not null; default:0" validate:"required, oneof=0 1 2"`
 	// 国家
-	Country string `gorm:"type:varchar(64); not null" validate:"min=1, max=128"`
+	Country string `gorm:"type:varchar(64)" validate:"min=1, max=128"`
 	// 省份
-	Province string `gorm:"type:varchar(64); not null" validate:"min=1, max=128"`
+	Province string `gorm:"type:varchar(64)" validate:"min=1, max=128"`
 	// 城市
-	City string `gorm:"type:varchar(64); not null" validate:"min=1, max=128"`
+	City string `gorm:"type:varchar(64)" validate:"min=1, max=128"`
 	// 语言
-	Language string `gorm:"type:varchar(32); not null" validate:"min=1, max=128"`
+	Language string `gorm:"type:varchar(32)" validate:"min=1, max=128"`
 
 	// 最后活跃时间
 	LastActive time.Time
